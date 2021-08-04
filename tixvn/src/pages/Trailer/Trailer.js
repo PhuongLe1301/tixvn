0e5bc268e987289d5019d30ade060b1b89fe99bf
Phuong <phuongle130199@gmail.com>
2021-08-04 04:33:43 -0700

    4/8

---
+++ tixvn/src/pages/Trailer/Trailer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Trailer.css'
+export default function Trailer(props) {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="close-btn">
+            <i className="fa fa-times-circle" onClick={()=>props.setTrigger(false)}></i>
+            </div>
+            {props.children}
+        </div>
+    ) : "";
+}
