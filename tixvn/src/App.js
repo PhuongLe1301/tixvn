835141662ca96627d85a747d331d425d606ae1e6
Hao <bnnhuhao@gmail.com>
2021-08-04 22:53:04 +0700

    admin chua co glassmorphism

--- tixvn/src/App.js
+++ tixvn/src/App.js
@@ -7,26 +7,24 @@ import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import CheckOut from './pages/CheckOut/CheckOut';
 import HomeTemplate from './template/HomeTemplate/HomeTemplate';
+import Login from './pages/Login/Login';
+import AdminTemplate from './template/AdminTemplate/AdminTemplate';
+import ManageMovie from './pages/ManageMovie/ManageMovie';
 
 export const history = createBrowserHistory();
 
 function App() {
   return (
-    // <BrowserRouter>
-    //  <Header/>
-    //     <Switch>
-
-    //       <Route exact path="/" component={Home} />
-    //       <Route exact path="/detail" component={Detail} />
-    //     </Switch>
-    
-    // </BrowserRouter>
     <Router history={history}>
       <Switch>
+        {/* Client  */}
         <HomeTemplate component={Home} exact path="/"/>
         <HomeTemplate component={Detail} exact path="/detail/:postId"/>
         <HomeTemplate component={CheckOut} exact path="/checkout/:id"/>
         <Route component={Register} exact path="/register"/>
+        <Route component={Login} exact path="/login"/>
+        {/* Admin  */}
+        <AdminTemplate component={ManageMovie} exact path="/admin/quanlyphim"/>
       </Switch>
     </Router>
   );
